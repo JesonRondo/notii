02a98b3fd602604bd6dc04db530baf4d75d00f12
buji <buji@mogujie.com>
2014-12-26 13:27:45 +0800

    about

--- app/fn/about.js
+++ app/fn/about.js
@@ -7,11 +7,19 @@ var
   data = require('../helper/data');
 
 module.exports = function *() {
+  var isfirst = false;
+
   var links = yield dao.links();
+
+  // 首次进入
+  if (!this.request.header.referer) {
+    isfirst = true;
+  }
 
   this.body = swig.renderFile('about.html', util._extend(data, {
     title: 'Halo Nice 2 meet u',
     links: links,
+    isfirst: isfirst,
     cate: '/about'
   }));
 };
