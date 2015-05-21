00d8cd0f9eed4bc88bb1dd7b677c5007c3e3f7bd
buji <buji@mogujie.com>
2015-05-21 13:18:31 +0800

    cdn

--- app/helper/data.js
+++ app/helper/data.js
@@ -9,7 +9,7 @@ module.exports = {
   domain: 'longzhou.me',
   cdnDomain: config.debug ? '' : config.cdnDomain,
   title: ' - Vic',
-  flag: md5.digest_s('祝你幸福还是给你幸福'),
+  flag: md5.digest_s('经历过了，就向前看吧'),
   navs: [{
     name: '首页',
     link: '/'
